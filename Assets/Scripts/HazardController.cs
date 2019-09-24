3380191e47206aedd5edddbd988ce520cd5a5bbc
Gam3R3ap3r <48007193+Gam3R3ap3r@users.noreply.github.com>
2019-09-24 17:20:55 +0800

    Hazard Controller
    +Script Added

---
+++ Assets/Scripts/HazardController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardController : MonoBehaviour
+{
+    WallScroll wScroll;
+    public float scrollAmount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wScroll = FindObjectOfType<WallScroll>();
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        scrollAmount = wScroll.pixelsPerTick;
+        transform.Translate(Vector2.up * (scrollAmount * Time.fixedDeltaTime), Space.World);
+    }
+}
+
