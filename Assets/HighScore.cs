323e6684c52584a533d5698f5832134ccaeb7da6
Krystal McDonald <krystalmc478@gmail.com>
2019-10-03 11:11:23 +0800

    fixed highscore

---
+++ Assets/HighScore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    int highscore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        highscore = (int)PlayerPrefs.GetFloat("HighScore", 0);
+        GetComponent<Text>().text = "HIGHSCORE: " + highscore.ToString() + "m";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
