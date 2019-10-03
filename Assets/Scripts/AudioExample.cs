9b9b4514af838475fbc7e8a856ffe578923dcce7
Gam3R3ap3r <48007193+Gam3R3ap3r@users.noreply.github.com>
2019-10-03 12:26:33 +0800

    Added Sound MIxer
    Audio Script and Sound Mixer added
    +Mixer Created
    +Script : SetVolume
    +Tagged Sawblade audio under SFX
    +Additional Sliders for audio control

---
+++ Assets/Scripts/AudioExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioExample : MonoBehaviour
+{
+
+    public AudioMixer myMixer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMixer.SetFloat("MusicVolume", -80.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SetVolume.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public Slider Vslider;
+    public Slider SFXslider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vslider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        SFXslider.value = PlayerPrefs.GetFloat("VFXVolume", 0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetLevelMusic()
+    {
+        float sliderValue = Vslider.value;
+        mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+
+    public void SetLevelSFX()
+    {
+        float sliderValue1 = SFXslider.value;
+        mixer.SetFloat("VFXVolume", Mathf.Log10(sliderValue1) * 20);
+        PlayerPrefs.SetFloat("VFXVolume", sliderValue1);
+    }
+}
